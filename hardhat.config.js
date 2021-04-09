96ade7dccdd1f9b6e336866c735fee351046776f
Zoemaestra <P2423891@my365.dmu.ac.uk>
2021-04-09 22:41:43 +0100

    Deployable to testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
-require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -23,6 +23,10 @@ module.exports = {
       url: 'http://localhost:8545',
       chainId: 31337,
       allowUnlimitedContractSize: true
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     },
   }
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const { parseEther } = ethers.utils;
+async function main() {
+
+    const [deployer] = await ethers.getSigners();
+  
+    console.log(
+      "Deploying contracts with the account:",
+      deployer.address
+    );
+    
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+    const Token = await ethers.getContractFactory("TestToken");
+    const token = await Token.deploy((parseEther('1000')));
+  
+    console.log("Token address:", token.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+  
